4c90695f347e3c3d22126c922dfb17253d31be59
kelvinou01 <kelvin.ou.jin.bin@gmail.com>
2022-12-20 19:48:56 +0800

    Store "source" field in articles

--- embed/models.py
+++ embed/models.py
@@ -5,15 +5,14 @@ from pydantic import HttpUrl
 class Article(BaseModel):
     title: str
     url: HttpUrl
+    source: str
     thumbnailUrl: HttpUrl
     publishedTime: datetime
     description: str
-
-class RawArticle(Article):
     content: str
 
-class RawArticles(BaseModel):
-    articles: list[RawArticle]
+class Articles(BaseModel):
+    articles: list[Article]
 
 class ProcessedArticle(Article):
     embeddings: list[float]
