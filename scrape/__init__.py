6b46e4fc3f31e4280dd99cc5aba4c0752d8c1b98
kelvinou01 <kelvin.ou.jin.bin@gmail.com>
2022-12-28 23:04:21 +0800

    Add missing type hints

--- scrape/__init__.py
+++ scrape/__init__.py
@@ -2,8 +2,9 @@
 import config
 import json
 import requests
-from embed.models import Article
 from meilisearch import Client
+
+from embed.models import Article
 
 def get_sources() -> list[str]:
     return json.loads(requests.get(config.SCRAPER_SOURCES_URL).content)
@@ -13,13 +14,13 @@ def is_duplicate(article: Article, client: Client) -> bool:
         "filter": [f"url = '{article['url']}'"]
     })
     return result['estimatedTotalHits'] > 0
-    
+
 def scrape_links(source: str) -> list[str]:
     response = requests.get(config.SCRAPER_LINKS_URL + f"?source={source}")
     response.raise_for_status()
     return json.loads(response.content)
 
-def scrape_article(url: str):
+def scrape_article(url: str) -> Article:
     response = requests.get(config.SCRAPER_ARTICLE_URL + f"?&url={url}")
     content = json.loads(response.content)
     if response.status_code != 200:
